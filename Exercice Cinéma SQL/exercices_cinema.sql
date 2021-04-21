f0f3ca43cbc4a924988f7b0674f12acdecb6f5d5
beatricemachalica <beatka.macha68@yahoo.fr>
2021-04-21 16:48:07 +0200

    update new exercise

--- Exercice Cinéma SQL/exercices_cinema.sql
+++ Exercice Cinéma SQL/exercices_cinema.sql
@@ -10,6 +10,12 @@ SELECT * FROM seance s, film f
 WHERE s.horaire LIKE '2021-04-21%'
 AND s.id_film = f.id_film
 AND f.titre = 'le roi lion'
+-- correction pour que la date soit toujours valable :
+SELECT DATE_FORMAT(s.horaire, '%H:%i') AS horaires, f.titre, s.id_salle
+FROM seance s
+INNER JOIN film f ON s.id_film = f.id_film
+WHERE f.titre = 'le roi lion'
+AND DATE(s.horaire) = DATE(NOW())
 
 -- c. Liste des films (mêmes champs que la question a) sans les horaires) qui durent plus de 2h
 SELECT f.id_film, f.titre, f.dateSortie, f.duree FROM film f
@@ -21,6 +27,13 @@ INNER JOIN film ON film.id_film = s.id_film
 INNER JOIN salle ON salle.id_salle = s.id_salle
 WHERE s.horaire > '2021-04-21'
 GROUP BY numSalle
+-- correction pour une meilleure approche avec la date
+SELECT COUNT(s.id_film), salle.id_salle AS numeroSalle
+FROM seance s
+INNER JOIN film ON film.id_film = s.id_film
+INNER JOIN salle ON salle.id_salle = s.id_salle
+WHERE DATE(s.horaire) > DATE(NOW())
+GROUP BY numeroSalle
 
 -- e. Donner la programmation du cinéma pour la période du 18/09/2019 au 24/09/2019
 SELECT * FROM seance s
@@ -32,3 +45,16 @@ INSERT INTO realisateur (nom, prenom) VALUES ('PHILIPS', 'Todd')
 -- g. Insérer le film « The Joker » (122 min – Réalisateur : Todd PHILIPS) et programmer ce 
 -- film le 09/10/2019 à 20h en salle 1 (le tarif de cette séance est fixé à 12€). Le film est 
 -- diffusé en VOSTF et ce n’est pas une avant-première.
+INSERT INTO film (titre, dateSortie, duree, id_realisateur) VALUES ('The Joker', '2019-01-01', 122, 6)
+INSERT INTO seance (id_salle, id_film, prix, langue, horaire, avantPremiere) VALUES (1, 9, 12, 'VOSTF', '2019-10-09 20:00:00', 0)
+
+-- BONUS : Numéro des salles qui diffusent 2 films ou plus
+SELECT s.id_salle AS numSalle, COUNT(s.id_film) AS nbFilm
+FROM seance s
+GROUP BY numSalle
+HAVING nbFilm >= 2
+
+-- BONUS : Augmenter la capacité de la salle 1 de 200 à 250 places
+UPDATE salle
+SET places = 250
+WHERE salle.id_salle = 1
